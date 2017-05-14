7678734b36f743a4f158d86cb4e4e9f55962c89c
vidyaramadani <vidyavio2412@gmail.com>
2017-05-14 21:58:55 +0700

    About Done

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl501/movieo/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl501/movieo/MainActivity.java
@@ -10,11 +10,11 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import id.sch.smktelkom_mlg.privateassignment.xirpl501.movieo.adapter.SourceAdapter;
+import layout.AboutusFragment;
 
 
 public class MainActivity extends AppCompatActivity
@@ -59,27 +59,8 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    //
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -95,11 +76,13 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_baru) {
             fragment = new baru();
-            setTitle("Baru");
+            setTitle("Top Movie");
         } else if (id == R.id.nav_camera) {
             fragment = new ImportFragment();
-            setTitle("Import");
+            setTitle("Coming Soon");
         } else if (id == R.id.nav_send) {
+            fragment = new AboutusFragment();
+            setTitle("About");
 
         }
 
---
+++ app/src/main/java/layout/AboutusFragment.java
@@ -0,0 +1,30 @@
+package layout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.sch.smktelkom_mlg.privateassignment.xirpl501.movieo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutusFragment extends Fragment {
+
+
+    public AboutusFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_aboutus, container, false);
+    }
+
+}
